4d0f0cb4ba07df94da7669ad7f14f4f289b809f7
marwabmr <marwa.boumour1@gmail.com>
2026-08-19 21:40:44 +0200

    update

--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,20 @@
 /** @type {import('next').NextConfig} */
+
 const nextConfig = {
+  output: 'export',
+
+  basePath: '/resume',
+  assetPrefix: '/resume/',
+  trailingSlash: true,
+
   typescript: {
     ignoreBuildErrors: true,
   },
+
   images: {
     unoptimized: true,
   },
- 
+
   eslint: {
     ignoreDuringBuilds: true,
   },
